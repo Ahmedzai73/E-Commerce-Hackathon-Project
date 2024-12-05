f1f3989c2b80defc7cde91eb7a765790e0b12d0f
Ahmed Zai <ahmedzai73@gmail.com>
2024-12-05 16:30:19 +0500

    breakpoints updated

--- wednesday-04-12-24/tailwind.config.ts
+++ wednesday-04-12-24/tailwind.config.ts
@@ -12,6 +12,15 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      screens: {
+        'xxs': '360px',   // Ultra small devices
+        'sm': '576px',    // Small devices
+        'md': '768px',    // Medium devices
+        'lg': '1024px',   // Large devices
+        'xl': '1200px',   // Extra large devices
+        '2xl': '1440px',  // Double extra large devices
+        'xxl': '1600px',  // Extra extra large devices
+      },
     },
   },
   plugins: [],
